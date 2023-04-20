0de1aa55a3e4d154ac6d5259ed72110e5c33656c
Mersio <mersiocarneiro87@gmail.com>
2023-04-20 00:31:23 -0300

    update

---
+++ 22 - JavaScript Moderno/22.07-spread_operator/scripts.js
@@ -0,0 +1,19 @@
+// Spread Operatior com arrays
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [6, 7, 8, 9, 10];
+const array3 = [...array1, ...array2];
+
+console.log(array3);
+
+const array4 = [0, ...array3, 11];
+
+console.log(array4);
+
+// Spread Operatior com objetos
+const carName = { name: "Gol" };
+const carBrand = { brand: "VW" };
+const otherInfos = { km: 100000, price: 49000 };
+
+const car = { ...carName, ...carBrand, ...otherInfos };
+
+console.log(car);
